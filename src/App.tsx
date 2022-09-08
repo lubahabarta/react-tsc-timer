5bb3c226a9ecc2f4556b2728a89f1dd3c2ba7564
lubahabarta <lubahabarta@gmail.com>
2022-09-08 20:51:33 +0200

    prvni kostra aplikace

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,39 @@
 import React from 'react';
 import './App.css';
+// react-router-dom
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    Redirect
+} from "react-router-dom";
 
 const App = () => {
     return (
         <div className="app">
-            hello world!
+            <Router>
+
+                <nav>
+                    <Link to="/Stopwatch">Stopwatch</Link>
+                    <Link to="/Timer">Timer</Link>
+                </nav>
+
+                <main>
+                    <Switch>
+                        <Route exact path="/">
+                            <Redirect to="/Stopwatch" />
+                        </Route>
+                        <Route path="/Stopwatch">
+                            <div>Stopwatch</div>
+                        </Route>
+                        <Route path="/Timer">
+                            <div>Timer</div>
+                        </Route>
+                    </Switch>
+                </main>
+
+            </Router>
         </div>
     );
 }
