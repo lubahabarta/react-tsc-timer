49ae0002cdc1fde66536b5d23f464082a85a93b9
lubahabarta <lubahabarta@gmail.com>
2022-09-09 23:38:19 +0200

    prekopal jsem celou navigaci zase

--- src/Stopwatch.tsx
+++ src/Stopwatch.tsx
@@ -1,19 +1,17 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
 
 const Stopwatch = () => {
-
-    const [path, setPath] = useState<string>(window.location.pathname);
 
     return (
         <div className="stopwatch">
             <nav>
                 <Link
-                    className={path === "/Stopwatch" ? "active" : ""}
+                    className={window.location.pathname === "/Stopwatch" ? "active" : ""}
                     to="/Stopwatch"
                 >Stopwatch</Link>
                 <Link
-                    className={path === "/Timer" ? "active" : ""}
+                    className={window.location.pathname === "/Timer" ? "active" : ""}
                     to="/Timer"
                 >Timer</Link>
             </nav>
